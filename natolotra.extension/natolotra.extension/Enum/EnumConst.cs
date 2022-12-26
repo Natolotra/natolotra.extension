eb43b392069b7a945a3487f09ded4e284bb57ed5
Tojo Ranaivozanany <ranaivozananytojo@outlook.fr>
2022-12-26 17:19:16 +0100

    3 Enum extension (ToInt, GetDesc, GetEnumValue)

---
+++ natolotra.extension/natolotra.extension/Enum/EnumConst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace natolotra.extension.Enum
+{
+    public static partial class EnumConst
+    {
+        public const string ENUM_TO_INT_MESSAGE_ARG_EXC = "T must be an enumerated type";
+        public const string ENUM_GET_DESC_MESSAGE_ARG_EXC = "T must be an enumerated type";
+        public const string ENUM_GET_DESC_MESSAGE_ARG_NUL_EXC = "T must be an argument not null";
+        public const string ENUM_GET_ENUM_VALUE_MESSAGE_ARG_EXC = "T must be an enumerated type";
+        public const string ENUM_GET_ENUM_VALUE_MESSAGE_ARG_VALUE_EXC = "T invalid value for enum";
+    }
+}
---
+++ natolotra.extension/natolotra.extension/Enum/EnumExtension.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace natolotra.extension.Enum
+{
+    /// <summary>
+    /// Extension class for enums
+    /// </summary>
+    public static partial class EnumExtension
+    {
+        /// <summary>
+        /// Extension method for enum to int
+        /// </summary>
+        /// <typeparam name="T">Enum type param</typeparam>
+        /// <param name="soure">Enum source</param>
+        /// <returns>Return int</returns>
+        /// <exception cref="ArgumentException">Argument exception if the attribute is not enum</exception>
+        public static int ToInt<T>(this T soure) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException(EnumConst.ENUM_TO_INT_MESSAGE_ARG_EXC);
+
+            return (int)(IConvertible)soure;
+        }
+
+        /// <summary>
+        /// Extension method for enum to take description
+        /// </summary>
+        /// <typeparam name="T">Enum type param</typeparam>
+        /// <param name="soure">Enum source</param>
+        /// <returns>Return string</returns>
+        /// <exception cref="ArgumentException">Argument exception if the argument is not enum</exception>
+        /// <exception cref="ArgumentNullException">Argument exception if the argument is null</exception>
+        public static string GetDesc<T>(this T soure) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException(EnumConst.ENUM_TO_INT_MESSAGE_ARG_EXC);
+
+            string? sourceToStr = soure.ToString();
+
+            if (sourceToStr == null)
+                throw new ArgumentNullException(nameof(soure), EnumConst.ENUM_GET_DESC_MESSAGE_ARG_NUL_EXC);
+
+            Type type = soure.GetType();
+            FieldInfo? fieldInfo = type != null ? type.GetField(sourceToStr) : null;
+            DescriptionAttribute? attribute = fieldInfo != null ? fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute), false) as DescriptionAttribute : null;
+
+            if (attribute != null)
+            { return attribute.Description; }
+            
+            return sourceToStr;
+        }
+
+        /// <summary>
+        /// Extension methode for string to take enum value
+        /// </summary>
+        /// <typeparam name="T">Enum type param</typeparam>
+        /// <param name="source">String source</param>
+        /// <returns>Return enum T</returns>
+        /// <exception cref="ArgumentException">Argument exception if the argument is not enum</exception>
+        /// <exception cref="ArgumentException">Argument exception if the argument is invalid value</exception>
+        public static T GetEnumValue<T>(this string source) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException(EnumConst.ENUM_GET_ENUM_VALUE_MESSAGE_ARG_EXC);
+            
+            T outResult;
+            var boolRes = System.Enum.TryParse<T>(source, true, out outResult);
+            if (boolRes)
+            { return outResult; }
+
+            throw new ArgumentException(EnumConst.ENUM_GET_ENUM_VALUE_MESSAGE_ARG_VALUE_EXC);
+        }
+    }
+}
